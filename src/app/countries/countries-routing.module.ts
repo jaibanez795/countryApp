1fcc0ce83b17a7ae785f3064f68a67f88d2b32ad
Jose <jaibanez795@gmail.com>
2024-08-09 17:49:30 -0600

    searchBox Module: Created

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: 'by-country', component: ByCountryPageComponent},
   {path: 'by-region', component: ByRegionPageComponent},
   {path: 'by/:id', component: CountryPageComponent},
-  {path: '**', redirectTo: ''}
+  {path: '**', redirectTo: 'by-capital'}
 ]
 
 @NgModule({
